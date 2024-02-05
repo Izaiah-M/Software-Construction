d7cef7d75837acfcd3f5a564a59abdf1b812d739
KatukundaRochelle <katukundarochelle16@gmail.com>
2024-02-05 23:34:29 +0300

    changes to solid code

--- SOLID_PRINCIPLE_ASSIGNMENT2/A94169/refactoring.py
+++ SOLID_PRINCIPLE_ASSIGNMENT2/A94169/refactoring.py
@@ -2,6 +2,9 @@ class Employee:
     def __init__(self, name, role):
         self.name = name
         self.role = role
-
+# Interface for report generation
+class ReportGenerator:
+    def generate_report(self, employee):
+        raise NotImplementedError("Subclasses must implement generate_report method")
 
 
