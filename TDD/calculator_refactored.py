fbb36781b35eba0436bd595bf5751e7eb25a8e7f
iz.aia.h <imukisa024@gmail.com>
2024-02-29 17:03:35 +0300

    test cases

--- TDD/calculator_refactored.py
+++ TDD/calculator_refactored.py
@@ -1,11 +1,8 @@
 
 from typing import Union
-=======
-
 
 class Calculator:
-
-    def _init_(self, num1: Union[int, float], num2: Union[int, float]):
+    def __init__(self, num1: Union[int, float], num2: Union[int, float]):
         self.num1 = num1
         self.num2 = num2
 
@@ -19,9 +16,24 @@ class Calculator:
     def multiply(self, x, y):
         return x * y
 
-    def divide(self, x, y):
-        if y == 0:
+    def divide(self):
+        if self.num2 == 0:
             raise ValueError("Cannot divide by zero!")
-        return x / y
+        return self.num1 / self.num2
+    
 
-   
+if __name__ == "__main__":
+    calc = Calculator(2, 5.5)
+
+    add = calc.add()
+    print(f"Added {calc.num1} and {calc.num2} to get: {add}")
+
+    subtract = calc.subtract()
+    print(f"Subtracted {calc.num1} and {calc.num2} to get: {add}")
+
+
+    divide = calc.divide()
+    print(f"Divided {calc.num1} and {calc.num2} to get: {add}")
+
+    multiply = calc.multiply()
+    print(f"Multiplied {calc.num1} and {calc.num2} to get: {add}")
